4acfed8a45fe17eff071b8091608bb77f967cbe9
Franco Papeschi <papeschifranco@gmail.com>
2025-07-18 15:33:44 -0300

    Creando forms parte 2

--- entregable_3/main/forms.py
+++ entregable_3/main/forms.py
@@ -2,19 +2,19 @@ from django import forms
 
 class consolasForm(forms.Form):
     modelo = forms.CharField(max_length=100)
-    empresa = forms.IntegerField(max_length=100)
+    empresa = forms.CharField(max_length=100)
     precio = forms.IntegerField()
     fecha_de_creacion = forms.DateField()
 
 
 class accesoriosForm(forms.Form):
     producto = forms.CharField(max_length=100)
-    empresa = forms.IntegerField(max_length=100)
+    empresa = forms.CharField(max_length=100)
     duracion_bateria = forms.IntegerField()
     precio = forms.IntegerField()
     
 
-class juegos(forms.Model):
+class juegos(forms.Form):
     juego = forms.CharField(max_length=100)
     empresa = forms.CharField(max_length=100)
     categoria = forms.CharField(max_length=100)
--- entregable_3/main/views.py
+++ entregable_3/main/views.py
@@ -1,4 +1,61 @@
 from django.shortcuts import render, redirect
+from .models import *
+from .forms import *
 
 def inicio(request):
     return render(request, "main/inicio.html")
+
+def agregar_consolas(request):
+    if request.method == 'POST':
+        form = consolasForm(request.POST)
+        if form.is_valid():
+            consolas = consolas(
+                #cleaned_data es lo que se guarda en el formulario
+                modelo=form.cleaned_data['modelo'],
+                empresa=form.cleaned_data['empresa'],
+                precio=form.cleaned_data['precio'],
+                fecha_de_creacion=form.cleaned_data['fecha_de_creacion'],
+            )
+            consolas.save()
+            return redirect('inicio')
+        
+    elif request.method == 'GET':
+        form = consolasForm()                                          #aca mandamo el formulario vacio
+        return render(request, "main/agregar_consolas.html", {"form": form})
+    
+
+    def agregar_juegos(request):
+        if request.method == 'POST':
+            #no se por me da advertencia
+            form = juegosForm(request.POST)
+            if form.is_valid():
+                juegos = juegos(
+                    #resivar esto
+                    nombre=form.cleaned_data['nombre'],
+                    genero=form.cleaned_data['genero'],
+                    precio=form.cleaned_data['precio'],
+                    fecha_de_lanzamiento=form.cleaned_data['fecha_de_lanzamiento'],
+                )
+                juegos.save()
+                return redirect('inicio')
+        elif request.method == 'GET':
+            #y aqui tambien
+            form = juegosForm()
+            return render(request, "main/agregar_juegos.html", {"form": form})
+
+    def agregar_accesorios(request):
+        if request.method == 'POST':
+            form = accesoriosForm(request.POST)
+            if form.is_valid():
+                accesorios = accesorios(
+                    #revisar esto
+                    nombre=form.cleaned_data['nombre'],
+                    tipo=form.cleaned_data['tipo'],
+                    precio=form.cleaned_data['precio'],
+                    compatibilidad=form.cleaned_data['compatibilidad'],
+                )
+                accesorios.save()
+                return redirect('inicio')
+        elif request.method == 'GET':
+            form = accesoriosForm()
+            return render(request, "main/agregar_accesorios.html", {"form": form})
