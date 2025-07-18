3613347b22a5e40df314234a8e2181e0811d9cf7
Franco Papeschi <papeschifranco@gmail.com>
2025-07-18 14:53:21 -0300

    completando requisitos

--- entregable_3/main/models.py
+++ entregable_3/main/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class consolas(models.Model):
+    modelo = models.CharField(max_length=100)
+    empresa = models.CharField(max_length=100)
+    precio = models.IntegerField()
+    fecha_de_creacion = models.DateField()
+
+    def __str__(self):
+        return f"{self.modelo} by {self.empresa}, Created: {self.fecha_de_creacion} -- ${self.precio}"
+
+
+class accesorios(models.Model):
+    producto = models.CharField(max_length=100)
+    empresa = models.CharField(max_length=100)
+    duracion_bateria = models.IntegerField()
+    precio = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.producto} by {self.empresa}, Bateria: {self.duracion_bateria} Horas -- ${self.precio}"
+
+
+class juegos(models.Model):
+    juego = models.CharField(max_length=100)
+    empresa = models.CharField(max_length=100)
+    categoria = models.CharField(max_length=100)
+    precio = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.juego} by {self.empresa}, Categoria: {self.categoria} -- ${self.precio}"
